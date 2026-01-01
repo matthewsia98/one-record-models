f9c6cee58ce2145fad49f16e5bd35769bd24b652
Matthew Sia <matthewsia98@gmail.com>
2026-01-01 14:09:53 -0500

    add notification test

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,11 @@ from pathlib import Path
 from devtools import debug
 from rdflib import Graph
 
-from one_record_ontology.models.generated.api import ServerInformation, Subscription
+from one_record_ontology.models.generated.api import (
+    Notification,
+    ServerInformation,
+    Subscription,
+)
 
 TEST_DIR = Path(__file__).parent.resolve()
 
@@ -31,6 +35,20 @@ def test_Subscription():
     g.parse(data=data, format="json-ld")
 
     obj = Subscription.from_graph(g)
+    debug(obj)
+
+    obj.to_graph()
+
+
+def test_Notification():
+    filepath = TEST_DIR / "resources" / "Notification.json"
+
+    data = Path(filepath).read_text()
+
+    g = Graph()
+    g.parse(data=data, format="json-ld")
+
+    obj = Notification.from_graph(g)
     debug(obj)
 
     obj.to_graph()
